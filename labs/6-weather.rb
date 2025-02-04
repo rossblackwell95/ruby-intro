a2d8bec57ff67fc0aa38507f80866cafacfcbb7b
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-02-04 16:59:19 +0000

    Lab 6 Completed

--- labs/6-weather.rb
+++ labs/6-weather.rb
@@ -30,3 +30,27 @@ weather_data = {
     { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
   ]
 }
+
+# Display current weather
+current_temp = weather_data[:current][:temperature]
+current_conditions = weather_data[:current][:conditions]
+
+puts "Currently it is #{current_temp} degrees and #{current_conditions}."
+
+# Display forecast for the next 7 days
+puts "Forecast for the next 7 days:"
+weather_data[:forecast].each do |day|
+  temp = day[:temperature]
+  conditions = day[:conditions]
+  precipitation = day[:precipitation]
+
+  # Print temperature and conditions
+  print "- #{temp} degrees and #{conditions}"
+
+  # Add precipitation details (if conditions met)
+  if precipitation > 0.5 && conditions != "Rainy"
+    print " with a #{(precipitation * 100).to_i}% chance of rain"
+  end
+
+  puts
+end
